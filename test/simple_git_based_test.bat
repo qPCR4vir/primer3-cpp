1c8a7974944e137367ab6d589576046fdeb2fcb4
qPCR4vir <arielvina@yahoo.es>
2018-08-07 22:07:54 +0200

    simple_git_based_test test2 passed

--- test/simple_git_based_test.bat
+++ test/simple_git_based_test.bat
@@ -1,2 +1,13 @@
 cd ../bin
+REM echo off
+echo Testing primer3_core.exe
+
+echo Test 1 : check '-output' and 'input_file'
 primer3_core.exe  --default_version=1 --strict --output=../test/primer_check_output ../test/primer_check_input
+echo Use git to see how the file primer_check_output changed
+pause
+
+echo Test 2 : check ' output' and ' input_file'
+primer3_core.exe  --default_version=1 --st > ../test/primer_check_output < ../test/primer_check_input
+echo Use git to see how the file primer_check_output changed
+pause
